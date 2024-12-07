a9702d0809541d068c0e77030fdf1435a42e32e8
emenshikova <emenshikova@vsegda-da.com>
2024-12-07 21:45:56 +0300

    day 2 ex 2

--- src/main/java/ru/menshikova/day2/Day2.java
+++ src/main/java/ru/menshikova/day2/Day2.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day2 {
     private static final String SEPARATOR = " ";
@@ -17,14 +19,50 @@ public class Day2 {
     public static void main(String[] args) throws IOException, URISyntaxException {
         List<String> text = FileReader.getText(DIR_NAME, FILE_NAME);
 
-        long secureLinesCount = text.stream()
+        List<int[]> unsecureLines = text.stream()
                 .map(line -> Arrays.stream(line.split(SEPARATOR))
                         .mapToInt(Integer::parseInt)
                         .toArray())
-                .filter(Day2::isLineSecure)
-                .count();
+                .filter(line -> !isLineSecure(line))
+                .collect(Collectors.toList());
+
+        long secureLinesCount = text.size() - unsecureLines.size();
 
         System.out.println(secureLinesCount);
+
+        long mySolution = secureLinesCount;
+        long chatGptSolution = secureLinesCount;
+
+        for (int[] unsecureLine : unsecureLines) {
+            for (int i = 0; i < unsecureLine.length; i++) {
+                int[] truncatedUnsecureLine = new int[unsecureLine.length - 1];
+                for (int j = 0, k = 0; j < unsecureLine.length; j++) {
+                    if (j != i) {
+                        truncatedUnsecureLine[k] = unsecureLine[j];
+                        k++;
+                    }
+                }
+                if (isLineSecure(truncatedUnsecureLine)) {
+                    mySolution += 1;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(mySolution);
+
+        chatGptSolution += unsecureLines.stream()
+                .filter(unsecureLine ->
+                        IntStream.range(0, unsecureLine.length)
+                                .mapToObj(i -> IntStream.range(0, unsecureLine.length)
+                                        .filter(j -> j != i)
+                                        .map(j -> unsecureLine[j])
+                                        .toArray())
+                                .anyMatch(Day2::isLineSecure)
+                )
+                .count();
+
+        System.out.println(chatGptSolution);
     }
 
     private static boolean isLineSecure(int[] line) {
